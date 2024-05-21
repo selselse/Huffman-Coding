f8b92d56a906bc646c368945bf412b9b1d8678bd
Limonatal <selmanerkurt2002@gmail.com>
2024-05-21 16:38:28 +0300

    Yesss, benim ilk kommitimgit add .!

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	girditxt := "girdi.txt"
+	girdi, err := os.Open(girditxt)
+	if err != nil {
+		fmt.Printf("Oh shit")
+		return
+	}
+	defer girdi.Close()
+}
